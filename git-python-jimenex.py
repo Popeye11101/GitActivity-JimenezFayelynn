2fd218c12c7360c9e1d41067d4cb50373f38830e
JimenezFayelynn <nnyl23sol@gmail.com>
2022-03-29 14:16:05 +0800

    My Educational Background added

---
+++ git-python-jimenex.py
@@ -0,0 +1,7 @@
+print ("JIMENEZ, Fayelynn S.")
+print ("April 23, 1999")
+print ("================================================")
+print ("Marinduque State College")
+print ("Bachelor of Science in Information Technology")
+print ("2nd year")
+print ("Section E")
